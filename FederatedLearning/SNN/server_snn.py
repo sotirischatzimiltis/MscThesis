205cda86f8357a6666bbee66a991e5bbd40afb04
sotirischatzimiltis <104900393+sotirischatzimiltis@users.noreply.github.com>
2022-07-16 12:51:02 +0100

    Add files via upload

--- FederatedLearning/SNN/server_snn.py
+++ FederatedLearning/SNN/server_snn.py
@@ -168,4 +168,9 @@ fl.server.start_server(
     server_address="localhost:8080",
     config={"num_rounds": 5},
     strategy=strategy,
+    certificates=(
+        Path(".cache/certificates/ca.crt").read_bytes(),
+        Path(".cache/certificates/server.pem").read_bytes(),
+        Path(".cache/certificates/server.key").read_bytes(),
+    )
 )
